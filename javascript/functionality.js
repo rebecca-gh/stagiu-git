5d9e6ec38d6c6289a518123ac5e7f9fe1d3e9e79
rebeca <rebeca.busuioc1@gmail.com>
2019-04-23 16:47:25 +0300

    toggle for array's items

--- javascript/functionality.js
+++ javascript/functionality.js
@@ -5,23 +5,53 @@ console.log(x);
 
 //function change() doesn't work for else...just doesn't work...IT WORKS WITH TOGGLE!!
 //I can't change color at every click!!  RESOLVED!!!
-//It doesn't work addEventListener
+//It doesn't work addEventListener -- only in console
+//I can't put the same function for every array's item -- resolved without toggle :( --resolved with toggle functions 
 function change() {
     var x = document.querySelector(" article p:first-child");
     x.classList.toggle('clicked');
 }
+function changeP() {
+    this.classList.toggle('biggerChanged');
+}
+document.querySelector('#p').addEventListener('click', changeP);
 
 function ttt(id) {
-    id.innerHTML = "i'm HERE!";
+    id.classList.toggle('header');
 }
 
 function btnChange(btn) {
     btn.classList.toggle('btnClicked');
     if (btn.className === 'btnClicked') {
-        btn.innerHTML = "I'm changed!"
+        btn.textContent = "I'm changed!"
     }
     else {
-        btn.innerHTML = "Change me!"
+        btn.textContent = "Change me!"
     }
 }
-
+var show = 0;
+function showHide(id, n) {
+    show ? showMe(id, n) : hideMe(id, n);
+}
+function showMe(id, n) {
+    var x = document.querySelectorAll('footer div div:last-child');
+    var len = x.length;
+    for (i = 0; i < len; i++) {
+        if (i == n) {
+            x[i].style.display = 'block';
+        }
+        id.textContent = 'Hide';
+    }
+    show = 0;
+}
+function hideMe(id, n) {
+    var x = document.querySelectorAll('footer div div:last-child');
+    var len = x.length;
+    for (i = 0; i < len; i++) {
+        if (i == n) {
+            x[i].style.display = 'none';
+        }
+        id.textContent = 'Show';
+    }
+    show = 1;
+}
