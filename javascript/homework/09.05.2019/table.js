e87859c389d75d15ae07ea76c9e5697e3a61582f
rebeca <rebeca.busuioc1@gmail.com>
2019-05-08 14:50:05 +0300

    table - without style

---
+++ javascript/homework/09.05.2019/table.js
@@ -0,0 +1,65 @@
+function create() {
+    var frag = document.createDocumentFragment();
+    var section = document.querySelector('section');
+    var table = document.createElement('table');
+    frag.appendChild(table);
+    section.appendChild(frag);
+
+
+    var products = [
+
+        { id: 1001, name: 'Apple iPhone X', price: 4000, description: '64GB, 4G' },
+        { id: 1002, name: 'Apple iPhone 7 plus', price: 2500, description: '32GB, black' },
+        { id: 1002, name: 'Apple iPhone XR', price: 3900, description: '64GB, 4G, blue' },
+        { id: 1002, name: 'Apple iPhone 8', price: 3000, description: '64GB, 4G, silver' },
+        { id: 1002, name: 'Samsung Galaxy S8', price: 2400, description: 'dual SIM, 64GB, 4G, black' },
+        { id: 1002, name: 'Samsung Galaxy S9', price: 2600, description: 'dual SIM, 64GB, 4G, blue' },
+        { id: 1002, name: 'Samsung Galaxy A7', price: 1000, description: 'dual SIM, 64GB, 4G, gold' },
+        { id: 1002, name: 'Lenovo K8 Note', price: 1200, description: 'dual SIM, 64GB, 4G, black' },
+    ]
+
+    console.log(frag);
+    console.log(section);
+
+    function createTable() {
+        function addTh() {
+            var row = document.createElement('tr');
+            var obj = products[0];
+            for (key in obj) {
+                var cell = document.createElement('td');
+                var text = document.createTextNode(key);
+                console.log(text);
+                cell.appendChild(text);
+                row.appendChild(cell);
+                // cell.setAttribute('style', 'color:red; border: 1px solid black; text-align:center');
+            }
+            table.appendChild(row);
+
+        }
+        function addBody() {
+            products.forEach(item => {
+                var row = document.createElement('tr');
+                for (key in item) {
+                    var cell = document.createElement('td');
+                    var text = document.createTextNode(item[key]);
+                    console.log(text);
+                    cell.appendChild(text);
+                    row.appendChild(cell);
+                }
+                table.appendChild(row);
+
+            })
+
+        }
+
+        addTh();
+        addBody();
+    }
+    createTable();
+
+    // function addStyle(){
+
+    // }
+
+}
+create();
